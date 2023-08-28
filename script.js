581548be83281ea84df8b1867667816adc2c6177
drkchu <derekdbchu@gmail.com>
2023-08-28 15:26:50 -0700

    Initialize basic structure of site

---
+++ script.js
@@ -0,0 +1,18 @@
+let operand0, operand1, operator;
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return y ? x / y : 'UNDEFINED';
+}
+
