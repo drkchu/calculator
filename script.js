5477a6fc727df1c9744e1dcedcbe66129a6a6fc9
drkchu <derekdbchu@gmail.com>
2023-08-28 17:39:50 -0700

    Set up HTML and CSS for the calculator, including button hovering animations

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ function divide(x, y) {
     return y ? x / y : 'UNDEFINED';
 }
 
+function operate(operator, x, y) {
+    switch (operator) {
+        case 'add':
+            return add(x, y);
+        case 'subtract':
+            return subtract(x, y);
+        case 'multiply':
+            return multiply(x, y);
+        case 'divide':
+            return divide(x, y);
+    }
+}
