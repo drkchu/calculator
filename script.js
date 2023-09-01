0d7491b37d07309c4fcc281b28019ec54e92df1f
drkchu <derekdbchu@gmail.com>
2023-08-31 20:18:20 -0700

    Finish clear and operation buttons - Just gotta finish the computing parts

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let [operand0, operand1, operator] = ['', '', ''];
+let [operand0, operand1, operator, result] = ['', '', '', ''];
 let user_input = document.querySelector('.user-input');
 
 function add(x, y) {
@@ -17,32 +17,85 @@ function divide(x, y) {
     return y ? x / y : 'UNDEFINED';
 }
 
+function modulus(x, y) {
+    return x % y;
+}
+
 function operate(operator, x, y) {
     switch (operator) {
-        case 'add':
+        case '+':
             return add(x, y);
-        case 'subtract':
+        case '-':
             return subtract(x, y);
-        case 'multiply':
+        case '×':
             return multiply(x, y);
-        case 'divide':
+        case '÷':
             return divide(x, y);
+        case '%':
+            return modulus(x, y);
     }
 }
 
 function initializeDigits() {
-    // access all buttons
     let buttons = Array.from(document.querySelectorAll('.digit'));
     buttons.forEach(button => {
         button.addEventListener('click', event => { // pass in a ref to a fn
-            operand0 += event.target.textContent;
-            updateDisplay();
+            if (!operand0 || !operator) {
+                if (event.target.textContent === '.' && operand0.includes('.')) return;
+                operand0 += event.target.textContent;
+                updateDisplay();
+            } else {
+                if (event.target.textContent === '.' && operand1.includes('.')) return;
+                operand1 += event.target.textContent;
+                updateDisplay();    
+            }
         });
     });
 }
 
 function updateDisplay() {
-    user_input.textContent = `${operand0} ${operator} ${operand1}`;
+    if (operand0 || operator || operand1) {
+        user_input.textContent = `${operand0} ${operator} ${operand1}`;
+    } else {
+        user_input.textContent = '\u00A0';
+    }
+}
+
+function initializeOperators() {
+    let buttons = Array.from(document.querySelectorAll('.operator'));    
+    buttons.forEach(button => {
+        button.addEventListener('click', event => {
+            operator = event.target.textContent;
+            updateDisplay();
+        });
+    });
+}
+
+function initializeClears() {
+    let buttonClear = document.querySelector('.clear');
+    let buttonAllClear = document.querySelector('.all-clear');
+
+    buttonClear.addEventListener('click', event => {
+        if (operand1) {
+            operand1 = operand1.slice(0, operand1.length - 1);
+            updateDisplay();
+            console.log('helo')
+        } else if (operator) {
+            operator = operator.slice(0, operator.length - 1);
+            updateDisplay();
+            console.log('helo2')   
+        } else if (operand0) {
+            operand0 = operand0.slice(0, operand0.length - 1);
+            updateDisplay();
+        }
+    });
+
+    buttonAllClear.addEventListener('click', event => {
+        [operand0, operand1, operator] = ['', '', ''];
+        updateDisplay();
+    });
 }
 
 initializeDigits();
+initializeOperators();
+initializeClears();
